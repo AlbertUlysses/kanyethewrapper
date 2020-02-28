f7b6420006848826e33f3f889d787af9f1f62b78
AlbertUlysses <albertulysseschavez@gmail.com>
2020-02-27 18:39:14 -0800

    docs: add docstring

--- kanye_the_wrapper.py
+++ kanye_the_wrapper.py
@@ -3,10 +3,9 @@ import requests
 
 class Kanye():
 
-    """
-    """
 
     def west(self) -> str:
+        """Returns a Kanye West quote."""
         r = requests.get('https://api.kanye.rest')
         json_call = r.json()
         diction = dict(json_call)
