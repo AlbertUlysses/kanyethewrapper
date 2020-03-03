6019b20fc423ab83c8510f3eb2ea41f278e76451
AlbertUlysses <albertulysseschavez@gmail.com>
2020-03-02 19:06:13 -0800

    refactor: remove library dependancy
    Updated the script to remove extra pip modules. Now the script can be ran using only the Python Standard Library. TODO: refactor names/code layout.

--- kanye_the_wrapper.py
+++ kanye_the_wrapper.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
-import requests
+import urllib.request 
+import json
 
 
 class Kanye():
 
-
     def west(self) -> str:
         """Returns a Kanye West quote."""
-        r = requests.get('https://api.kanye.rest')
-        json_call = r.json()
-        diction = dict(json_call)
-        payload = str(diction['quote'])
-        return payload 
-
+        location = 'https://api.kanye.rest'
+        req_call = urllib.request.Request(location, headers={"User-Agent": "Mozilla/5.0"})
+        content = urllib.request.urlopen(req_call)
+        read_content = content.read()
+        payload = json.loads(read_content.decode("utf-8"))
+        convert_json = dict(payload)
+        str_quote = str(convert_json['quote'])
+        return str_quote 
 
 
