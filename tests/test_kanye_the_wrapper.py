525bb2176c8332c0f11257a5f0607fbf7a5356f1
AlbertUlysses <albertulysseschavez@gmail.com>
2020-05-30 10:50:28 -0700

    test: add test to bound_2 method

--- tests/test_kanye_the_wrapper.py
+++ tests/test_kanye_the_wrapper.py
@@ -1,18 +1,27 @@
 import pytest
+from kanye_the_wrapper.kanye_the_wrapper import Kanye
 
 
 def test_west():
     pass
 
+
 def test_bound_2():
-    pass
+    test_class = Kanye()
+    actual = test_class.bound_2(10)
+    expected = 'The dictionary can save 10 quotes.'
+    assert actual == expected
+    actual = test_class.bound_2(5)
+    expected = 'The dictionary can save 5 quotes.'
+    assert actual == expected
+    
 
 def test_watch_the_throne():
     pass
 
+
 def test_heard_em_say():
-    from kanye_the_wrapper.kanye_the_wrapper import Kanye
-    test_variable = Kanye()
+    test_class = Kanye()
     with pytest.raises(KeyError) as exception_info:
-        test_variable.heard_em_say()
+        test_class.heard_em_say()
     assert exception_info.match('The dictionary is empty.')
